d2f0344f6d0a3a6eb0f17ddd32af4e07090f8582
Francisco André <franciscoandre.farp@gmail.com>
2023-12-18 03:29:09 +0000

    update home banner

--- front/fitnessfront/src/pages/Home.tsx
+++ front/fitnessfront/src/pages/Home.tsx
@@ -1,10 +1,18 @@
-import banner from "../assests/anastase-maragos-9dzWZQWZMdE-unsplash.jpg";
+import banner from "../assests/anastase-maragos-9dzWZQWZMdE-unsplash 2.png";
 import Header from "../components/Header";
 function Home() {
   return (
     <div className="Home">
       <Header />
-      <img src={banner} alt="logo" width={1920}></img>
+      <div className="relative">
+        <img src={banner} alt="Banner" className="w-full h-auto" />
+        <div className="absolute top-1/2 left-5 transform -translate-y-1/2 text-center text-white">
+          <h1 className="text-4xl font-bold text-react-blue">
+            BECOME THE BEST VERSION OF YOURSELF
+          </h1>
+          <button>Start now!</button>
+        </div>
+      </div>
     </div>
   );
 }
